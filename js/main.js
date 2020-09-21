6739eda48447c1a28c61ba51f6dc76ca32c095f3
juweek <Prototype23@msn.com>
2020-09-21 08:52:48 -0400

    a few changes

--- js/main.js
+++ js/main.js
@@ -1,44 +1,21 @@
 var startButton = document.getElementById('br-body__start-button')
 var restartButton = document.getElementById('br-body__restart-button')
-var inputField = document.getElementById('br-body__input-field')
-var inputLines = document.getElementById('br-body__info-lines')
-
 /*
 ------------------------------
 Set variables for the circle
 ------------------------------
 */
-var greenColor = '#3db540'
-var redColor = '#c70000'
-
 var circleWidth = 300
 var circleHeight = 300
 var circleRadius = Math.min(circleHeight, circleWidth) / 2
-var canvasArea = circleWidth * circleHeight
-
-var margin = {
-  top:10,
-  left: 60,
-  right: 20,
-  bottom: 40
-}
-var lineWidth = 300 - margin.left - margin.right
-var lineHeight = 250 - margin.top - margin.bottom
-
 
 /*
 ------------------------------
 set global variables for the methods
 ------------------------------
 */
-var totalInCircle = 0
 var totalPoints = 0
 var radius = 4
-
-var totalPointsDrawn = 0
-var pointsInCircle = 0
-var estimatedPi = 0
-var currentError = 0
 
 /*
 ------------------------------
@@ -92,20 +69,19 @@ var drawCircle = () => {
 METHOD: draw the actual dot
 ------------------------------
 */
-var drawPoint = (x, y, r) => {
+var drawPoint = (x, y) => {
   context.beginPath()
   context.arc(x, y, radius, 0, 2 * Math.PI)
   context.closePath()
   context.fill()
 }
 
-
 /*
 ------------------------------
 METHOD: change the text everytime a new point is drawn
 ------------------------------
 */
-var updateText = () => {
+var updateText = (totalPointsDrawn, totalInCircle, estimatedPi) => {
   document.getElementById('total-iteration').innerHTML = totalPointsDrawn.toString()
   document.getElementById('in-circle').innerHTML = totalInCircle.toString()
   document.getElementById('pi-estimate').innerHTML = estimatedPi.toFixed(5)
@@ -118,6 +94,7 @@ METHOD: check if the text field is empty or a number
 ------------------------------
 */
 var validateText = () => {
+  var inputField = document.getElementById('br-body__input-field')
   if(inputField.value == "" || isNaN(inputField.value) || parseInt(inputField.value) <= 0) {
     alert('Please type in a number greater than 0')
     return false
@@ -135,14 +112,13 @@ METHOD: event listener for the start and restart button. depending on which, res
 */
 restartButton.addEventListener('click', (o) => {
   if(validateText()) {
-    totalPointsDrawn = 0
-    totalInCircle = 0
     drawCircle()
     start()
   }
 })
 
 startButton.addEventListener('click', (o) => {
+  var inputLines = document.getElementById('br-body__info-lines')
   if(validateText()) {
     startButton.innerHTML = 'Add more'
     inputLines.style.opacity = 1
@@ -158,46 +134,44 @@ METHOD: iterate over every dot. call the update method to draw it
 ------------------------------
 */
 var start = () => {
+
+  //set variables
   var count = 1
-  var drawInterval = setInterval(function() {
-    if(count >= totalPoints) {
-      clearInterval(drawInterval)
-    }
-    update(count)
-    count++
-  }, 5)
+  var totalPointsDrawn = 0
+  var totalInCircle = 0
+  var estimatedPi = 0
+  var drawInterval = setInterval(update, 5)
 
   startButton.addEventListener('click', null)
   restartButton.addEventListener('click', null)
-}
 
+  var update = () => {
+    //first, validate that you arent finished
+    if(count >= totalPoints) {
+      clearInterval(drawInterval)
+    }
+    //second, generate a random x and y number to figure out the next dot's coordinates
+    var x = randomInt(0, circleWidth)
+    var y = randomInt(0, circleHeight)
 
-/*
-------------------------------
-METHOD: draw a dot and determine if it is in the circle
-------------------------------
-*/
-var update = (count) => {
-  //first, generate a random x and y number to figure out the next dot's coordinates
-  var x = randomInt(0, circleWidth)
-  var y = randomInt(0, circleHeight)
+    //check if the point is in the circle using euclideanDistance
+    if (euclideanDistance(x, circleWidth / 2, y, circleHeight / 2) < circleRadius) {
+      context.fillStyle = '#3db540'
+      totalInCircle++
+    }
+    else {
+      context.fillStyle = '#c70000'
+    }
 
-  //check if the point is in the circle using euclideanDistance
-  if (euclideanDistance(x, circleWidth / 2, y, circleHeight / 2) < circleRadius) {
-    context.fillStyle = greenColor
-    totalInCircle++
+    //aincrease counter, calculate new values
+    count++
+    totalPointsDrawn++
+    estimatedPi = (totalInCircle/totalPointsDrawn) * 4
+
+    //draw a point and update the text
+    drawPoint(x, y, radius)
+    updateText(totalPointsDrawn, totalInCircle, estimatedPi)
   }
-  else {
-    context.fillStyle = redColor
-  }
-
-  totalPointsDrawn++
-  estimatedPi = (totalInCircle/totalPointsDrawn) * 4
-  currentError = Math.PI - estimatedPi
-
-  //draw a point and update the text
-  drawPoint(x, y, radius)
-  updateText()
 }
 
 
